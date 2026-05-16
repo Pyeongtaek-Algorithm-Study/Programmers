c8892f91a370b01abcad88e3982563113bcb9405
SunM <43372170+Ssroid@users.noreply.github.com>
2026-05-16 23:00:29 +0900

    Refact. 코드 수정

--- level_2/디펜스_게임/solve.java
+++ level_2/디펜스_게임/solve.java
@@ -3,18 +3,21 @@ import java.util.*;
 class Solution {
 
     public int solution(int n, int k, int[] enemy) {
-
-        if (k >= enemy.length) {
-            return enemy.length;
+        
+        int finalRound = enemy.length;
+        
+        // 무적권만으로 모든 라운드 도달 가능
+        if (k >= finalRound) {
+            return finalRound;
         }
 
-        int ans = 0;
+        int ans = finalRound;
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1);
 
         for (int i = 0; i < enemy.length; i++) {
 
-            // 우선 무적권 후보에 추가
+            // 우선 무적권 사용
             pq.offer(enemy[i]);
 
             // 무적권 개수 초과 시 가장 작은 라운드를 병사 처리
