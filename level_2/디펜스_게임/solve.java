e370a6c188a90e4d0c1d3eb9f55ecafe22afec42
SunM <coex0113@gmail.com>
2026-05-16 22:29:45 +0900

    Feat. 디펜스 게임 java 풀이 추가

---
+++ level_2/디펜스_게임/solve.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+
+    public int solution(int n, int k, int[] enemy) {
+
+        int ans = enemy.length;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < enemy.length; i++) {
+
+            // 우선 무적권 후보에 추가
+            pq.offer(enemy[i]);
+
+            // 무적권 개수 초과 시 가장 작은 라운드를 병사 처리
+            if (pq.size() > k) {
+                n -= pq.poll();
+            }
+
+            // 병사 부족 시 종료
+            if (n < 0) {
+                ans = i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
